d8b9a70708497a78836f2a2c38d5656affad76f1
okdruid <tomcw@outlook.com>
2023-01-26 06:32:01 +0000

    [curator] update list.lua

--- list.lua
+++ list.lua
@@ -5,23 +5,42 @@ local _, t = ...
 t.version = "@project-version@"
 t.case_table = {
 	[0] = {
-		name = "Okshift",
-		class = "DRUID",
-		guid = "Player-4465-0307DFA7",
-		faction = "Horde",
-		category = "dungeon",
-		level = 4,
+		category = "trade",
+		level = 3,
 		description = "Snuck into gdkp while they were distracted and pickpocketed all the golds",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
+		players = {
+			[0] = {
+				name = "Okshift",
+				class = "DRUID",
+				guid = "Player-4465-0307DFA7",
+				faction = "Horde",
+			},
+			[1] = {
+				name = "Oksneak",
+				class = "ROGUE",
+				faction = "Horde",
+			}
+		}
 	},
 	[1] = {
-		name = "Oksneak",
-		class = "ROGUE",
-		faction = "Horde",
 		category = "raid",
 		level = 2,
 		description = "Slayed all bosses then walked away with all the fancy loots",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
+		players = {
+			[0] = {
+				name = "Ow",
+				class = "ROGUE",
+				faction = "Alliance",
+				aliases = { "Owls", "Owley" },
+			},
+			[1] = {
+				name = "Ok",
+				class = "PALADIN",
+				faction = "Horde",
+			}
+		}
 	},
 	[2] = {
 		category = "dungeon",
