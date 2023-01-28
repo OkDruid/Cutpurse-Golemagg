64de11c6469b5ca465436c8107ac55c80ccc6424
okdruid <tomcw@outlook.com>
2023-01-28 11:32:01 +0000

    [curator] update list.lua

--- list.lua
+++ list.lua
@@ -5,18 +5,23 @@ local _, t = ...
 t.version = "@project-version@"
 t.case_table = {
 	[0] = {
-		category = "trade",
-		level = 3,
+		category = "gdkp",
+		level = 2,
 		description = "Snuck into gdkp while they were distracted and pickpocketed all the golds",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
 		players = {
 			[0] = {
+				name = "Ok",
+				class = "PALADIN",
+				faction = "Horde",
+			},
+			[1] = {
 				name = "Okshift",
 				class = "DRUID",
 				guid = "Player-4465-0307DFA7",
 				faction = "Horde",
 			},
-			[1] = {
+			[2] = {
 				name = "Oksneak",
 				class = "ROGUE",
 				faction = "Horde",
@@ -24,40 +29,36 @@ t.case_table = {
 		}
 	},
 	[1] = {
-		category = "raid",
+		category = "dungeon",
 		level = 2,
 		description = "Slayed all bosses then walked away with all the fancy loots",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
 		players = {
 			[0] = {
-				name = "Ow",
-				class = "ROGUE",
-				faction = "Alliance",
-				aliases = { "Owls", "Owley" },
+				guild = "Sons of Hey",
+				faction = "Horde",
 			},
 			[1] = {
-				name = "Ok",
-				class = "PALADIN",
+				name = "Swedge",
+				class = "PRIEST",
 				faction = "Horde",
+				aliases = { "Swedgley" },
+			},
+			[2] = {
+				name = "Ow",
+				class = "ROGUE",
+				faction = "Horde",
+				aliases = { "Owls", "Owley" },
 			}
 		}
 	},
 	[2] = {
-		category = "dungeon",
+		name = "Ok",
+		class = "PALADIN",
+		faction = "Horde",
+		category = "raid",
 		level = 3,
 		description = "sneaked into the dungeon and vanished with all the loot!",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
-		players = {
-			[0] = {
-				name = "Oksneak",
-				class = "ROGUE",
-				faction = "Horde",
-			},
-			[1] = {
-				name = "Ok",
-				class = "PALADIN",
-				faction = "Horde",
-			}
-		}
 	}
 }
