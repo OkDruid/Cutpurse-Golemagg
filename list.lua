5ca3b1864f69eaee7d08eaaa55149bdc9b1a6d67
okdruid <tomcw@outlook.com>
2023-02-09 22:57:10 +0000

    [curator] update list.lua

--- list.lua
+++ list.lua
@@ -6,61 +6,65 @@ t.version = "@project-version@"
 
 t.case_table = {
 	[0] = {
+		name = "Swedge",
+		class = "PRIEST",
+		faction = "Horde",
+		aliases = {"Swedgley"},
+		category = "dungeon",
+		level = 3,
+		description = "Did some super swedgey stuff then logged out!",
+		url = "https://discord.gg/",
+	},
+	[1] = {
+		name = "Ow",
+		class = "ROGUE",
+		faction = "Alliance",
+		aliases = {"Owls", "Owley"},
+		category = "raid",
+		level = 3,
+		description = "Did some really super sneaky stuff all right.",
+		url = "https://discord.gg/",
+	},
+	[2] = {
 		name = "Ok",
 		class = "PALADIN",
-		faction = "Horde",
+		faction = "Alliance",
+		aliases = {"Äk", "ßoss"},
 		category = "raid",
 		level = 3,
 		description = "listed for being too damn good at everything!",
 		url = "https://discord.gg/",
 	},
-	[1] = {
+	[3] = {
 		category = "gdkp",
-		level = 4,
+		level = 3,
 		description = "Snuck into gdkp while they were distracted and pickpocketed all the golds",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
 		players = {
 			[0] = {
-				name = "Ok",
-				class = "PALADIN",
-				faction = "Horde",
-			},
-			[1] = {
-				name = "Swedge",
-				class = "PRIEST",
-				faction = "Horde",
-				aliases = { "Swedgley" },
-			},
-			[2] = {
 				name = "Oksneak",
 				class = "ROGUE",
 				faction = "Horde",
-			}
-		}
-	},
-	[2] = {
-		category = "gdkp",
-		level = 3,
-		description = "Slayed all bosses then walked away with all the fancy loots",
-		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
-		players = {
-			[0] = {
-				name = "Ok",
-				class = "PALADIN",
-				faction = "Horde",
 			},
 			[1] = {
-				name = "Ow",
-				class = "ROGUE",
+				name = "Ok",
+				class = "PALADIN",
 				faction = "Alliance",
-				aliases = { "Owls", "Owley" },
+				aliases = {"Äk", "ßoss"},
+			},
+			[2] = {
+				name = "Swedge",
+				class = "PRIEST",
+				faction = "Horde",
+				aliases = {"Swedgley"},
 			}
 		}
 	},
-	[3] = {
+	[4] = {
 		name = "Ok",
 		class = "PALADIN",
-		faction = "Horde",
+		faction = "Alliance",
+		aliases = {"Äk", "ßoss"},
 		category = "gdkp",
 		level = 3,
 		description = "sneaked into the dungeon and vanished with all the loot!",
