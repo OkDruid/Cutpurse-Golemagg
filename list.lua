00bee3a198d79e62fec932fc46d4dd22996b81c4
okdruid <tomcw@outlook.com>
2023-03-07 10:25:01 +0000

    [curator] update list.lua

--- list.lua
+++ list.lua
@@ -6,6 +6,51 @@ t.version = "@project-version@"
 
 t.case_table = {
 	[0] = {
+		name = "test4",
+		class = "MAGE",
+		faction = "Horde",
+		category = "gdkp",
+		level = 3,
+		description = "some description longer than 40 characters and shorter than 140..",
+		url = "https://discord.gg/",
+	},
+	[1] = {
+		name = "test7",
+		class = "PRIEST",
+		faction = "Horde",
+		category = "trade",
+		level = 3,
+		description = "test report with test description with filler text..",
+		url = "https://discord.gg/",
+	},
+	[2] = {
+		name = "test3",
+		class = "MAGE",
+		faction = "Horde",
+		category = "trade",
+		level = 3,
+		description = "test report with test description with filler text..",
+		url = "https://discord.gg/",
+	},
+	[3] = {
+		name = "test7",
+		class = "PRIEST",
+		faction = "Horde",
+		category = "dungeon",
+		level = 3,
+		description = "test report with test description with filler text..",
+		url = "https://discord.gg/",
+	},
+	[4] = {
+		name = "test5",
+		class = "WARRIOR",
+		faction = "Horde",
+		category = "raid",
+		level = 3,
+		description = "test report with test description with filler text..",
+		url = "https://discord.gg/",
+	},
+	[5] = {
 		name = "test7",
 		class = "PRIEST",
 		faction = "Horde",
@@ -14,30 +59,30 @@ t.case_table = {
 		description = "drunkenly stumbled into the dungeon and ran off with all the shinies.",
 		url = "https://discord.gg/",
 	},
-	[1] = {
+	[6] = {
 		category = "dungeon",
 		level = 3,
 		description = "drunkenly stumbled into the dungeon and ran off with all the shinies.",
 		url = "https://discord.gg/",
 		players = {
 			[0] = {
-				name = "test6",
-				class = "ROGUE",
+				name = "test7",
+				class = "PRIEST",
 				faction = "Horde",
 			},
 			[1] = {
-				name = "test5",
-				class = "PALADIN",
+				name = "test6",
+				class = "SHAMAN",
 				faction = "Horde",
 			},
 			[2] = {
-				name = "test7",
-				class = "PRIEST",
+				name = "test5",
+				class = "WARRIOR",
 				faction = "Horde",
 			}
 		}
 	},
-	[2] = {
+	[7] = {
 		category = "trade",
 		level = 4,
 		description = "drunkenly stumbled into the dungeon and ran off with all the shinies.",
@@ -45,91 +90,62 @@ t.case_table = {
 		players = {
 			[0] = {
 				name = "test5",
-				class = "PALADIN",
+				class = "WARRIOR",
 				faction = "Horde",
 			},
 			[1] = {
 				name = "test6",
-				class = "ROGUE",
+				class = "SHAMAN",
 				faction = "Horde",
 			}
 		}
 	},
-	[3] = {
-		category = "dungeon",
-		level = 3,
-		description = "drunkenly stumbled into the dungeon and ran off with all the shinies.",
-		url = "https://discord.gg/",
-		players = {
-			[0] = {
-				name = "test2",
-				class = "SHAMAN",
-				faction = "Alliance",
-			},
-			[1] = {
-				name = "test5",
-				class = "PALADIN",
-				faction = "Horde",
-			},
-			[2] = {
-				name = "test4",
-				class = "PALADIN",
-				faction = "Horde",
-			},
-			[3] = {
-				name = "test1",
-				class = "SHAMAN",
-				faction = "Horde",
-			},
-			[4] = {
-				name = "test6",
-				class = "ROGUE",
-				faction = "Horde",
-			}
-		}
-	},
-	[4] = {
-		category = "trade",
+	[8] = {
+		category = "gdkp",
 		level = 3,
 		description = "Did some really super sneaky stuff all right.",
 		url = "https://discord.gg/",
 		players = {
 			[0] = {
 				name = "Ow",
-				class = "WARRIOR",
-				faction = "Horde",
+				class = "MAGE",
+				faction = "Alliance",
 				aliases = {"Owls", "Owley"},
 			},
 			[1] = {
-				name = "Ok",
-				faction = "Alliance",
-				aliases = {"Äk", "ßoss"},
+				name = "Äonahala",
+				class = "PALADIN",
+				faction = "Horde",
+				aliases = {"Äk", "ßoss", "Ok", "Äok"},
 			}
 		}
 	},
-	[5] = {
-		name = "Ok",
-		faction = "Alliance",
-		aliases = {"Äk", "ßoss"},
+	[9] = {
+		name = "Äonahala",
+		class = "PALADIN",
+		faction = "Horde",
+		aliases = {"Äk", "ßoss", "Ok", "Äok"},
 		category = "trade",
-		level = 4,
+		level = 3,
 		description = "sneaked into the dungeon and vanished with all the loot!",
 		url = "https://discord.com/channels/1055067923829510174/1058910793577529365/1066386586306084964",
 	},
-	[6] = {
-		name = "Ok",
-		faction = "Alliance",
-		aliases = {"Äk", "ßoss"},
+	[10] = {
+		name = "Äonahala",
+		class = "PALADIN",
+		faction = "Horde",
+		aliases = {"Äk", "ßoss", "Ok", "Äok"},
 		category = "harassment",
 		level = 3,
 		description = "some description longer than 40 characters",
 		url = "https://discord.gg",
 	},
-	[7] = {
-		name = "Ok",
-		faction = "Alliance",
-		aliases = {"Äk", "ßoss"},
-		category = "plebs",
+	[11] = {
+		name = "Äonahala",
+		class = "PALADIN",
+		faction = "Horde",
+		aliases = {"Äk", "ßoss", "Ok", "Äok"},
+		category = "plebasaurs",
 		level = 3,
 		description = "listed for being too damn good at everything!",
 		url = "https://discord.gg/",
