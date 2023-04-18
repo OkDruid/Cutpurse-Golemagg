fc4ef4da4aa802acb4b9c28dcaab6f7d59ffb133
okdruid <tomcw@outlook.com>
2023-04-18 11:26:01 +0100

    [curator] update list.lua

--- list.lua
+++ list.lua
@@ -7,92 +7,120 @@ local _, t = ...
 t.version = "@project-version@"
 
 t.case_table = {
-	[0] = {
-		name = "Sabertooth",
-		faction = "Horde",
-		aliases = {"Mammoth", "Test", "Trex", "Raptor", "test7"},
+	[1] = {
 		category = "raid",
 		level = 2,
 		description = "test report with test description with filler text..",
 		url = "https://discord.gg/",
+		name = "Sabertooth",
+		faction = "Horde",
+		aliases = {
+			"Mammoth",
+			"Test",
+			"Trex",
+			"Raptor",
+			"test7",
+		},
 	},
-	[1] = {
+	[2] = {
 		category = "dinos",
 		level = 3,
 		description = "drunkenly stumbled into the dungeon and ran off with all the shinies.",
 		url = "https://discord.gg/",
 		players = {
-			[0] = {
+			[1] = {
 				name = "test5",
 				class = "WARRIOR",
 				faction = "Horde",
 			},
-			[1] = {
+			[2] = {
 				name = "test6",
 				class = "SHAMAN",
 				faction = "Horde",
 			},
-			[2] = {
+			[3] = {
 				name = "Sabertooth",
 				faction = "Horde",
-				aliases = {"Mammoth", "Test", "Trex", "Raptor", "test7"},
-			}
-		}
+				aliases = {
+					"Mammoth",
+					"Test",
+					"Trex",
+					"Raptor",
+					"test7",
+				},
+			},
+		},
 	},
-	[2] = {
+	[3] = {
 		category = "trade",
 		level = 2,
 		description = "Did some really super sneaky stuff all right.",
 		url = "https://discord.gg/",
 		players = {
-			[0] = {
+			[1] = {
 				name = "Swedge",
 				faction = "Horde",
-				aliases = {"Swedgley"},
+				aliases = {
+					"Swedgley",
+				},
 			},
-			[1] = {
+			[2] = {
 				name = "Ayoah",
 				class = "PALADIN",
 				faction = "Horde",
-				aliases = {"Ayoh", "Äonahala", "Äk", "ßoss", "Ok", "Äok"},
-			}
-		}
+				aliases = {
+					"Ayoh",
+					"Äonahala",
+					"Äk",
+					"ßoss",
+					"Ok",
+					"Äok",
+				},
+			},
+		},
 	},
-	[3] = {
+	[4] = {
 		category = "raid",
 		level = 3,
 		description = "some description longer than 40 characters",
 		url = "https://discord.gg",
 		players = {
-			[0] = {
+			[1] = {
 				name = "Ayoah",
 				class = "PALADIN",
 				faction = "Horde",
-				aliases = {"Ayoh", "Äonahala", "Äk", "ßoss", "Ok", "Äok"},
+				aliases = {
+					"Ayoh",
+					"Äonahala",
+					"Äk",
+					"ßoss",
+					"Ok",
+					"Äok",
+				},
 			},
-			[1] = {
+			[2] = {
 				name = "Knightni",
 				class = "DEATHKNIGHT",
 				faction = "Horde",
-			}
-		}
+			},
+		},
 	},
-	[4] = {
-		name = "test4",
-		class = "MAGE",
-		faction = "Horde",
+	[5] = {
 		category = "raid",
 		level = 3,
 		description = "some description longer than 40 characters and shorter than 140..",
 		url = "https://discord.com/channels/1055067923829510174/1073620620761382942/1082603411192676423",
-	},
-	[5] = {
-		name = "test6",
-		class = "SHAMAN",
+		name = "test4",
+		class = "MAGE",
 		faction = "Horde",
+	},
+	[6] = {
 		category = "gdkp",
 		level = 4,
 		description = "stole all the cookies from the cookie jar!",
 		url = "https://discord.gg/",
-	}
+		name = "test6",
+		class = "SHAMAN",
+		faction = "Horde",
+	},
 }
